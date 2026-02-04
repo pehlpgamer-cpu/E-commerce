593c60adcefefe910d5ca1ef905545d08ff7c561
Paulo E. Heymann <pehlpgamer@gmail.com>
2026-02-04 04:01:04 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Business 
+- Thrift Shop
+
+# Teck-stack 
+- HTML
+- Vanilla Javascript
+- Tailwind CSS v3 - CDN installation
+- Bootstrap Icons
---
+++ js/components/navSideBar.js
@@ -0,0 +1,28 @@
+export function navSideBar() {
+    
+    let btns = '';
+    
+    let navSideBar = 
+    `
+    <nav>
+        <button>
+            <i class=""></i>
+        </button>
+        <section>
+            ${btns}
+        </section>
+    </nav>
+    
+    `;
+}
+
+function navBtn(title, iconClassName, pageLink, noTitle = false)
+{
+
+    let btn = 
+    `
+
+    `;
+    return btn;
+}
+
